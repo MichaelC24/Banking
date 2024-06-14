26a70ace5d882b8d6e2b49f05088b66051ae2135
Michael Clemons <Mclemons@tql.com>
2024-06-14 15:56:14 -0400

    Add project files.

---
+++ Banking/BankingInfo.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    internal class BankingInfo
+    {
+        public int AccountId { get; set; }
+        public string Description { get; set; }
+        public decimal Balance { get; set; }
+//----------------------------------------------------//
+//---------------Methods------------------------------//
+//----------------------------------------------------//
+        public void Deposit()
+        {
+            decimal deposit = 0;
+
+            while (true)
+            {
+                Console.Write($"How much would you like to deposit here is the current balance {Balance}: ");
+                string userInput = Console.ReadLine();
+
+                if (deposit < 0)
+                {
+                    Console.WriteLine("You can only deposit a positive number into your account please only enter a positive number.");
+                }
+                else
+                {
+                    deposit = Convert.ToInt32(userInput);
+                    Balance += deposit;
+                    break;
+                }
+            }
+            Console.WriteLine($"Here is your current balance: {Balance}");
+        }
+        public void Withdraw()
+        {
+            decimal withdraw = 0;
+
+
+            while (true)
+            {
+                Console.Write($"How much would you like to withdraw this is your current balance {Balance}: ");
+                string userInput = Console.ReadLine();
+
+                if (withdraw > Balance)
+                {
+                    Console.WriteLine("The withdraw is greater than the balance please enter a number less than your balance.");
+                }
+                else
+                {
+                    withdraw = Convert.ToInt32(userInput);
+                    Balance -= withdraw;
+                    break;
+                }
+            }
+            Console.WriteLine($"Here is your current balance: {Balance}");
+        }
+        public void curBalance()
+        {
+            Console.WriteLine($"Here is your current Balance {Balance}");
+        }
+        public void Transfer()
+        {
+            //BankingInfo bankingInfo = new BankingInfo();
+            Program program = new Program();
+
+            Console.Write("What is the account ID you would like to transfer to: ");
+            string transferId = Console.ReadLine();
+
+            int endTransferId = Convert.ToInt32(transferId);
+
+            foreach (BankingInfo otherIdBankingInfo in Program.bankInfo)
+            {
+                int otherId = otherIdBankingInfo.AccountId;
+
+                if (otherIdBankingInfo.AccountId == otherId)
+                {
+                    Console.Write($"Okay how much would you like to transfer your balance is {Balance}: ");
+                    string transferAmountString = Console.ReadLine();
+
+                    int transferAmt = Convert.ToInt32(transferAmountString);
+
+                    otherIdBankingInfo.Balance = transferAmt;
+
+                    Console.WriteLine($"Money Transfered your balance is: {Balance - transferAmt}");
+                    //Console.WriteLine($"Other Account balance {otherIdBankingInfo.Balance}");
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ Banking/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Data;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+
+namespace Banking
+{
+    internal class Program
+    {
+        public static List<BankingInfo> bankInfo = new List<BankingInfo>();
+        static void Main(string[] args)
+        {
+            BankingInfo bankingInfo = new BankingInfo();
+
+            bankInfo.Add(new BankingInfo { AccountId = 1, Balance = 0, Description = "Checking" });
+            bankInfo.Add(new BankingInfo { AccountId = 2, Balance = 0, Description = "Checking" });
+
+            Console.WriteLine("Please type out a command for example (Deposit 100): ");
+            string firstInput = Console.ReadLine();
+            string[] splitInput = firstInput.Split(" ");
+            string Command = splitInput[0];
+            string amount = splitInput[1];
+
+            while (true)
+            {
+                Console.Write("What would you like to do Deposit, Withdraw, Current Balance, transfer, or if your all done please enter exit?: ");
+                string userInput = Console.ReadLine();
+
+                switch (Command.ToLower())
+                {
+                    case "deposit":
+
+                        bankingInfo.Deposit();
+                        break;
+
+                    case "withdraw":
+                        bankingInfo.Withdraw();
+                        break;
+
+                    case "current balance":
+                        bankingInfo.curBalance();
+                        break;
+                    case "transfer":
+                        bankingInfo.Transfer();
+                        break;
+
+                }
+                if (userInput.ToLower() == "exit")
+                {
+                    break;
+                }
+            }
+
+
+
+
+        }
+    }
+
+}
+
+   //Console.Write("What is the account ID you would like to transfer to: ");
+                        //string transferId = Console.ReadLine();
+
+                        //int endTransferId = Convert.ToInt32(transferId);
+
+                        //foreach (BankingInfo otherIdBankingInfo in bankInfo)
+                        //{
+                        //    int otherId = otherIdBankingInfo.AccountId;
+
+                        //    if (otherIdBankingInfo.AccountId == otherId)
+                        //    {
+                        //        Console.Write($"Okay how much would you like to transfer your balance is {bankingInfo.Balance}: ");
+                        //        string transferAmountString = Console.ReadLine();
+
+                        //        int transferAmt = Convert.ToInt32(transferAmountString);
+
+                        //        otherIdBankingInfo.Balance = transferAmt;
+
+                        //        Console.WriteLine("Money Transfered");
+                        //        //Console.WriteLine($"Other Account balance {otherIdBankingInfo.Balance}");
+                        //        break;
+                        //    }
+                        //}
